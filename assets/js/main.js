09dad840cc0c3f7ff216614b48dfea30b3af26db
Pratik Thanki <thankip9@outlook.com>
2018-02-11 22:07:07 +0000

    new images 2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							// 'images/bg01.jpg': 'center',
+							'images/bg01.jpg': 'center',
 							// 'images/bg02.jpg': 'center',
 							// 'images/bg03.jpg': 'center',
 							'images/bg04.jpeg': 'center',
