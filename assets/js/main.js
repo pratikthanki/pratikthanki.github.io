d53a1f5a5629e836c24ecfc4aa828f8fac3ad293
Pratik Thanki <thankip9@outlook.com>
2019-07-23 08:38:24 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,174 +1,186 @@
-(function () {
+(function() {
 
-	"use strict";
+    "use strict";
 
-	// Methods/polyfills.
+    // Methods/polyfills.
 
-	// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
-	!function () { function t(t) { this.el = t; for (var n = t.className.replace(/^\s+|\s+$/g, "").split(/\s+/), i = 0; i < n.length; i++)e.call(this, n[i]) } function n(t, n, i) { Object.defineProperty ? Object.defineProperty(t, n, { get: i }) : t.__defineGetter__(n, i) } if (!("undefined" == typeof window.Element || "classList" in document.documentElement)) { var i = Array.prototype, e = i.push, s = i.splice, o = i.join; t.prototype = { add: function (t) { this.contains(t) || (e.call(this, t), this.el.className = this.toString()) }, contains: function (t) { return -1 != this.el.className.indexOf(t) }, item: function (t) { return this[t] || null }, remove: function (t) { if (this.contains(t)) { for (var n = 0; n < this.length && this[n] != t; n++); s.call(this, n, 1), this.el.className = this.toString() } }, toString: function () { return o.call(this, " ") }, toggle: function (t) { return this.contains(t) ? this.remove(t) : this.add(t), this.contains(t) } }, window.DOMTokenList = t, n(Element.prototype, "classList", function () { return new t(this) }) } }();
+    // classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
+    ! function() {
+        function t(t) { this.el = t; for (var n = t.className.replace(/^\s+|\s+$/g, "").split(/\s+/), i = 0; i < n.length; i++) e.call(this, n[i]) }
 
-	// canUse
-	window.canUse = function (p) { if (!window._canUse) window._canUse = document.createElement("div"); var e = window._canUse.style, up = p.charAt(0).toUpperCase() + p.slice(1); return p in e || "Moz" + up in e || "Webkit" + up in e || "O" + up in e || "ms" + up in e };
+        function n(t, n, i) { Object.defineProperty ? Object.defineProperty(t, n, { get: i }) : t.__defineGetter__(n, i) } if (!("undefined" == typeof window.Element || "classList" in document.documentElement)) { var i = Array.prototype,
+                e = i.push,
+                s = i.splice,
+                o = i.join;
+            t.prototype = { add: function(t) { this.contains(t) || (e.call(this, t), this.el.className = this.toString()) }, contains: function(t) { return -1 != this.el.className.indexOf(t) }, item: function(t) { return this[t] || null }, remove: function(t) { if (this.contains(t)) { for (var n = 0; n < this.length && this[n] != t; n++);
+                        s.call(this, n, 1), this.el.className = this.toString() } }, toString: function() { return o.call(this, " ") }, toggle: function(t) { return this.contains(t) ? this.remove(t) : this.add(t), this.contains(t) } }, window.DOMTokenList = t, n(Element.prototype, "classList", function() { return new t(this) }) } }();
 
-	// window.addEventListener
-	(function () { if ("addEventListener" in window) return; window.addEventListener = function (type, f) { window.attachEvent("on" + type, f) } })();
+    // canUse
+    window.canUse = function(p) { if (!window._canUse) window._canUse = document.createElement("div"); var e = window._canUse.style,
+            up = p.charAt(0).toUpperCase() + p.slice(1); return p in e || "Moz" + up in e || "Webkit" + up in e || "O" + up in e || "ms" + up in e };
 
-	// Vars.
-	var $body = document.querySelector('body');
+    // window.addEventListener
+    (function() { if ("addEventListener" in window) return;
+        window.addEventListener = function(type, f) { window.attachEvent("on" + type, f) } })();
 
-	// Disable animations/transitions until everything's loaded.
-	$body.classList.add('is-loading');
+    // Vars.
+    var $body = document.querySelector('body');
 
-	window.addEventListener('load', function () {
-		window.setTimeout(function () {
-			$body.classList.remove('is-loading');
-		}, 100);
-	});
+    // Disable animations/transitions until everything's loaded.
+    $body.classList.add('is-loading');
 
-	// Slideshow Background.
-	(function () {
+    window.addEventListener('load', function() {
+        window.setTimeout(function() {
+            $body.classList.remove('is-loading');
+        }, 100);
+    });
 
-		// Settings.
-		var settings = {
+    // Slideshow Background.
+    (function() {
 
-			// Images (in the format of 'url': 'alignment').
-			images: {
-				'images/bg01.jpg': 'center',
-				'images/bg02.jpg': 'center',
-				'images/bg03.png': 'center',
-				'images/bg04.jpg': 'center',
-				'images/bg05.jpg': 'center',
-				'images/bg06.jpg': 'center',
-				'images/bg07.jpg': 'center',
-				'images/bg08.jpg': 'center',
-				'images/bg09.jpg': 'center',
-			},
+        // Settings.
+        var settings = {
 
-			// Delay.
-			delay: 6000
+            // Images (in the format of 'url': 'alignment').
+            images: {
+                // 'images/bg01.jpg': 'center',
+                'images/bg02.jpg': 'center',
+                'images/bg03.png': 'center',
+                'images/bg04.jpg': 'center',
+                'images/bg05.jpg': 'center',
+                // 'images/bg06.jpg': 'center',
+                // 'images/bg07.jpg': 'center',
+                // 'images/bg08.jpg': 'center',
+                // 'images/bg09.jpg': 'center',
+            },
 
-		};
+            // Delay.
+            delay: 6000
 
-		// Vars.
-		var pos = 0, lastPos = 0,
-			$wrapper, $bgs = [], $bg,
-			k, v;
+        };
 
-		// Create BG wrapper, BGs.
-		$wrapper = document.createElement('div');
-		$wrapper.id = 'bg';
-		$body.appendChild($wrapper);
+        // Vars.
+        var pos = 0,
+            lastPos = 0,
+            $wrapper, $bgs = [],
+            $bg,
+            k, v;
 
-		for (k in settings.images) {
+        // Create BG wrapper, BGs.
+        $wrapper = document.createElement('div');
+        $wrapper.id = 'bg';
+        $body.appendChild($wrapper);
 
-			// Create BG.
-			$bg = document.createElement('div');
-			$bg.style.backgroundImage = 'url("' + k + '")';
-			$bg.style.backgroundPosition = settings.images[k];
-			$wrapper.appendChild($bg);
+        for (k in settings.images) {
 
-			// Add it to array.
-			$bgs.push($bg);
+            // Create BG.
+            $bg = document.createElement('div');
+            $bg.style.backgroundImage = 'url("' + k + '")';
+            $bg.style.backgroundPosition = settings.images[k];
+            $wrapper.appendChild($bg);
 
-		}
+            // Add it to array.
+            $bgs.push($bg);
 
-		// Main loop.
-		$bgs[pos].classList.add('visible');
-		$bgs[pos].classList.add('top');
+        }
 
-		// Bail if we only have a single BG or the client doesn't support transitions.
-		if ($bgs.length == 1
-			|| !canUse('transition'))
-			return;
+        // Main loop.
+        $bgs[pos].classList.add('visible');
+        $bgs[pos].classList.add('top');
 
-		window.setInterval(function () {
+        // Bail if we only have a single BG or the client doesn't support transitions.
+        if ($bgs.length == 1 ||
+            !canUse('transition'))
+            return;
 
-			lastPos = pos;
-			pos++;
+        window.setInterval(function() {
 
-			// Wrap to beginning if necessary.
-			if (pos >= $bgs.length)
-				pos = 0;
+            lastPos = pos;
+            pos++;
 
-			// Swap top images.
-			$bgs[lastPos].classList.remove('top');
-			$bgs[pos].classList.add('visible');
-			$bgs[pos].classList.add('top');
+            // Wrap to beginning if necessary.
+            if (pos >= $bgs.length)
+                pos = 0;
 
-			// Hide last image after a short delay.
-			window.setTimeout(function () {
-				$bgs[lastPos].classList.remove('visible');
-			}, settings.delay / 2);
+            // Swap top images.
+            $bgs[lastPos].classList.remove('top');
+            $bgs[pos].classList.add('visible');
+            $bgs[pos].classList.add('top');
 
-		}, settings.delay);
+            // Hide last image after a short delay.
+            window.setTimeout(function() {
+                $bgs[lastPos].classList.remove('visible');
+            }, settings.delay / 2);
 
-	})();
+        }, settings.delay);
 
-	// Signup Form.
-	(function () {
+    })();
 
-		// Vars.
-		var $form = document.querySelectorAll('#signup-form')[0],
-			$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
-			$message;
+    // Signup Form.
+    (function() {
 
-		// Bail if addEventListener isn't supported.
-		if (!('addEventListener' in $form))
-			return;
+        // Vars.
+        var $form = document.querySelectorAll('#signup-form')[0],
+            $submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+            $message;
 
-		// Message.
-		$message = document.createElement('span');
-		$message.classList.add('message');
-		$form.appendChild($message);
+        // Bail if addEventListener isn't supported.
+        if (!('addEventListener' in $form))
+            return;
 
-		$message._show = function (type, text) {
+        // Message.
+        $message = document.createElement('span');
+        $message.classList.add('message');
+        $form.appendChild($message);
 
-			$message.innerHTML = text;
-			$message.classList.add(type);
-			$message.classList.add('visible');
+        $message._show = function(type, text) {
 
-			window.setTimeout(function () {
-				$message._hide();
-			}, 3000);
+            $message.innerHTML = text;
+            $message.classList.add(type);
+            $message.classList.add('visible');
 
-		};
+            window.setTimeout(function() {
+                $message._hide();
+            }, 3000);
 
-		$message._hide = function () {
-			$message.classList.remove('visible');
-		};
+        };
 
-		// Events.
-		// Note: If you're *not* using AJAX, get rid of this event listener.
-		$form.addEventListener('submit', function (event) {
+        $message._hide = function() {
+            $message.classList.remove('visible');
+        };
 
-			event.stopPropagation();
-			event.preventDefault();
+        // Events.
+        // Note: If you're *not* using AJAX, get rid of this event listener.
+        $form.addEventListener('submit', function(event) {
 
-			// Hide message.
-			$message._hide();
+            event.stopPropagation();
+            event.preventDefault();
 
-			// Disable submit.
-			$submit.disabled = true;
+            // Hide message.
+            $message._hide();
 
-			// Process form.
-			// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-			// but there's enough here to piece together a working AJAX submission call that does.
-			window.setTimeout(function () {
+            // Disable submit.
+            $submit.disabled = true;
 
-				// Reset form.
-				$form.reset();
+            // Process form.
+            // Note: Doesn't actually do anything yet (other than report back with a "thank you"),
+            // but there's enough here to piece together a working AJAX submission call that does.
+            window.setTimeout(function() {
 
-				// Enable submit.
-				$submit.disabled = false;
+                // Reset form.
+                $form.reset();
 
-				// Show message.
-				$message._show('success', 'Thank you!');
-				//$message._show('failure', 'Something went wrong. Please try again.');
+                // Enable submit.
+                $submit.disabled = false;
 
-			}, 750);
+                // Show message.
+                $message._show('success', 'Thank you!');
+                //$message._show('failure', 'Something went wrong. Please try again.');
 
-		});
+            }, 750);
 
-	})();
+        });
+
+    })();
 
 })();
