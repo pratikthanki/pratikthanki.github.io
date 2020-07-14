7c3bfcfbffc572e744a0d5752cfe0db0951cd18e
Pratik Thanki <pratikthanki1@gmail.com>
2020-07-14 20:03:20 +0100

    Remove old images and add to assets dir.

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,13 +64,11 @@
 
             // Images (in the format of 'url': 'alignment').
             images: {
-                'images/bg01.jpg': 'center',
-                'images/bg02.jpg': 'center',
-                'images/bg03.png': 'center',
-                'images/bg04.jpg': 'center',
-                'images/bg05.jpg': 'center',
-                'images/bg06.jpg': 'center',
-                'images/bg07.jpg': 'center',
+                'assets/images/bg01.jpg': 'center',
+                'assets/images/bg02.jpg': 'center',
+                'assets/images/bg03.png': 'center',
+                'assets/images/bg04.jpg': 'center',
+                'assets/images/bg05.jpg': 'center'
             },
 
             // Delay.
