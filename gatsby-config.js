4d43e52005bb73f10426d5066ffe5eb80ab381fe
Pratik Thanki <pratikthanki1@gmail.com>
2020-07-19 15:35:55 +0100

    Update icon and refactor config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,8 @@ module.exports = {
         siteImage: `/banner.jpg`,
         author: `@pratik_thanki`
     },
-    plugins: [{
+    plugins: [
+        {
             resolve: `@lekoarts/gatsby-theme-minimal-blog`,
             options: {
                 basePath: `/`,
@@ -62,32 +63,20 @@ module.exports = {
                 trackingId: `UA-118874438-1`,
             },
         },
-        `gatsby-plugin-sitemap`,
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-                short_name: `minimal-blog`,
-                description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+                name: `Pratik`,
+                short_name: `Pratik`,
+                description: `I write about stuff.`,
+                lang: `en`,
                 start_url: `/`,
                 background_color: `#fff`,
                 theme_color: `#6B46C1`,
                 display: `standalone`,
-                icons: [{
-                        src: `/android-chrome-192x192.png`,
-                        sizes: `192x192`,
-                        type: `image/png`,
-                    },
-                    {
-                        src: `/android-chrome-512x512.png`,
-                        sizes: `512x512`,
-                        type: `image/png`,
-                    },
-                ],
+                icon: 'static/favicon.png',
             },
         },
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-netlify`,
         shouldAnalyseBundle && {
             resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
             options: {
@@ -96,5 +85,8 @@ module.exports = {
                 openAnalyzer: false,
             },
         },
+        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-netlify`,
     ].filter(Boolean),
 }
